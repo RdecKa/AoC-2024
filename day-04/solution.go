ab5391fcba0e87b8259efa343c23f5abfe77244e
RdecKa <17461665+RdecKa@users.noreply.github.com>
2024-12-04 23:44:32 +0100

    Day 4 - star 1

---
+++ day-04/solution.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/RdecKa/AoC-2024/util"
+	"github.com/dlclark/regexp2"
+)
+
+const xmasLen = 4
+
+func constructRegexs(gridSize int) [8]*regexp2.Regexp {
+	reRight := regexp2.MustCompile(`(XMAS)`, 0)
+	reLeft := regexp2.MustCompile(`(SAMX)`, 0)
+
+	vertBreak := fmt.Sprintf(`[\S\s]{%d}`, gridSize)
+	reDown := regexp2.MustCompile(fmt.Sprintf(`(?=(X%sM%sA%sS))`, vertBreak, vertBreak, vertBreak), 0)
+	reUp := regexp2.MustCompile(fmt.Sprintf(`(?=(S%sA%sM%sX))`, vertBreak, vertBreak, vertBreak), 0)
+
+	// Diagonal top-left <=> botton-right
+	diagRBreak := fmt.Sprintf(`[\S\s]{%d}`, gridSize+1)
+	ensureRSpace := fmt.Sprintf(`[\S]{%d}[\S\s]{%d}`, xmasLen-1, gridSize+1-(xmasLen-1))
+	reRightDown := regexp2.MustCompile(fmt.Sprintf(`(?=(X%sM%sA%sS))`, ensureRSpace, diagRBreak, diagRBreak), 0)
+	reLeftUp := regexp2.MustCompile(fmt.Sprintf(`(?=(S%sA%sM%sX))`, ensureRSpace, diagRBreak, diagRBreak), 0)
+
+	// Diagonal top-rigt <=> botton-left
+	diagLBreak := fmt.Sprintf(`[\S\s]{%d}`, gridSize-1)
+	ensureLSpace := fmt.Sprintf(`[\S\s]{%d}`, xmasLen-1)
+	reLeftDown := regexp2.MustCompile(fmt.Sprintf(`(?=(%sX%sM%sA%sS))`, ensureLSpace, diagLBreak, diagLBreak, diagLBreak), 0)
+	reRightUp := regexp2.MustCompile(fmt.Sprintf(`(?=(%sS%sA%sM%sX))`, ensureLSpace, diagLBreak, diagLBreak, diagLBreak), 0)
+
+	result := [8]*regexp2.Regexp{reRight, reLeft, reDown, reUp, reRightDown, reLeftUp, reLeftDown, reRightUp}
+	return result
+}
+
+func countMatches(re *regexp2.Regexp, s string) (count int) {
+	m, _ := re.FindStringMatch(s)
+	for m != nil {
+		count++
+		m, _ = re.FindNextMatch(m)
+	}
+	return
+}
+
+func star1(inputString string, gridSize int) (count int) {
+	res := constructRegexs(gridSize)
+	for _, re := range res {
+		count += countMatches(re, inputString)
+	}
+	return
+}
+
+func star2(inputString string, gridSize int) (count int) {
+	return
+}
+
+func gridSize(inputString string) int {
+	return len(strings.Split(inputString, "\n")[0])
+}
+
+func solution(starFunc func(string, int) int, input int) int {
+	inputFileName := util.InputFileName(input)
+	inputString := util.FileToString(inputFileName)
+	return starFunc(inputString, gridSize(inputString))
+}
+
+func Star1(input int) int {
+	return solution(star1, input)
+}
+func Star2(input int) int {
+	return solution(star2, input)
+}
+
+func main() {
+	const input = 0
+	fmt.Println(Star1(input))
+	fmt.Println(Star2(input))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/RdecKa/AoC-2024
 
 go 1.23.3
+
+require github.com/dlclark/regexp2 v1.11.4 // indirect
